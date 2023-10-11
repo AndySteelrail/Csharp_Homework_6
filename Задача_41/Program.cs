0e80b09a1eb0760626b0b5b151cfa0fc6a2eb484
Andrey Murugov <murugov_git@mail.ru>
2023-10-11 21:14:22 +0700

    Add files-tasks

---
+++ Задача_41/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		double[] InputNumbers()
+        {
+            Console.WriteLine("Введите числа через запятую");
+            string input = String.Empty + Console.ReadLine();
+            string[] inputs = input.Split(',');
+            double[] inputNumbers = new double[inputs.Length];
+            for (int i=0; i<inputs.Length; i++)
+                {inputNumbers[i] = Double.Parse(inputs[i]);}
+
+            return inputNumbers[];
+        }
+
+
+
+	}
+}
