1e36ec0653cbff21c000be1d007d54e924204433
Andrey Murugov <murugov_git@mail.ru>
2023-10-11 21:56:13 +0700

    Finished first task

--- Задача_41/Program.cs
+++ Задача_41/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+using System;
 					
 public class Program
 {
@@ -6,17 +11,34 @@ public class Program
 	{
 		double[] InputNumbers()
         {
-            Console.WriteLine("Введите числа через запятую");
+            Console.WriteLine("Введите числа через точку с запятой");
             string input = String.Empty + Console.ReadLine();
-            string[] inputs = input.Split(',');
+            string[] inputs = input.Split(';');
             double[] inputNumbers = new double[inputs.Length];
             for (int i=0; i<inputs.Length; i++)
                 {inputNumbers[i] = Double.Parse(inputs[i]);}
 
-            return inputNumbers[];
+            return inputNumbers;
         }
 
+        int CountPositiveNumbers(double[] array)
+        {
+            int sumOfPositiveNumbers = 0;
+            for (int i = 0; i<array.Length; i++)
+                {if (array[i] > 0) sumOfPositiveNumbers++;}
+            return sumOfPositiveNumbers;
+        }
 
+        void OutputMessage(int sumOfPositiveNumbers)
+        {
+            Console.WriteLine(
+                "В веденном массиве " + 
+                sumOfPositiveNumbers +
+                " положительных чисел");
+        }
 
+        double[] inputNumbers = InputNumbers();
+        int sumOfPositiveNumbers = CountPositiveNumbers(inputNumbers);
+        OutputMessage(sumOfPositiveNumbers);
 	}
 }
